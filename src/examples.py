c22cd2c741ae2013b5d0dc9253453be3858eaaae
Justin Schachter <jschach@umich.edu>
2021-12-03 18:52:06 -0500

    adding self to member helper methods

--- src/examples.py
+++ src/examples.py
@@ -1,9 +1,9 @@
 import ads7828
 
 ## Test throwing an address error exception: 
-# adc_0 = ads7828.ADS7828(0x89)
+# adc = ads7828.ADS7828(0x89)
 
-adc_0 = ads7828.ADS7828(0x48)
-
+adc = ads7828.ADS7828(0x48)
+print(adc._read_channel_single_ended_5())
 while True:
     adc_0._self_test_single_ended_iref_on_ad_on()
